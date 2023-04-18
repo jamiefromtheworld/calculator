83fbb4c2947ebb8d1f04ace519b1f2394da50dd9
Your Name <you@example.com>
2023-04-18 14:01:48 -0700

    still working

--- calculator.js
+++ calculator.js
@@ -1,17 +1,6 @@
-//now i have constant values with names so when i say "zero" the number 0 is referenced
-const zero = 0;
-const one = 1;
-const two = 2;
-const three = 3;
-const four = 4;
-const five = 5;
-const six = 6;
-const seven = 7;
-const eight = 8;
-const nine = 9;
-
-
 function screenValue(button) {
     document.getElementById('screen').innerHTML = button;
-
 }
+
+
+
